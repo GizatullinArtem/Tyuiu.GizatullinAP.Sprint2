d99adaa3d2e18a1a61002b900fb0e0053c0cc4cd
coc1986data@gmail.com <coc1986data@gmail.com>
2024-10-30 19:30:53 +0500

    Task7

---
+++ Tyuiu.GizatullinAP.Sprint2.Task7.V9/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.GizatullinAP.Sprint2.Task7.V9.Lib;
+namespace Tyuiu.GizatullinAP.Sprint2.Task7.V9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Гизаутллин А. П. | ИСПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по шаблону                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #9                                                              *");
+            Console.WriteLine("* Выполнил: Гизатуллин Артём Павлович | ИСПб-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает исходные данные                 *");
+            Console.WriteLine("* (вещественные значения) и вычисляет , находится ли точка с              *");
+            Console.WriteLine("* координатами X,Y в заштрихованной области.                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            DataService ds = new DataService();
+            Console.Write("Введите Х: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(res);           
+
+
+        }
+    }
+}
